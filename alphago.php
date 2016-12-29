4785cd1077e906067a4726a78a573e6893dca2ae
Marc <marc.bru.peace@gmail.com>
2016-12-29 18:04:57 +0100

    debut du remplissage des pages

--- alphago.php
+++ alphago.php
@@ -1,4 +1,6 @@
 <?php require 'header.php'; ?>
     <body>
+		<h1>AlphaGo</h1>
+			<p>AlphaGo est un programme informatique capable de jouer au jeu de go, développé par l'entreprise britannique Google DeepMind.En octobre 2015, il devient le premier programme à battre un joueur professionnel (le français Fan Hui) sur un goban de taille normale (19x19) sans handicap.Le terme est né en 1995 aux États-Unis dans un article publié par Rosalind Picard, une chercheuse du MIT qui considérait qu’il fallait que les robots nous ressemblent sur le plan émotionnel pour mieux nous servir. L’informatique affective peut se définir comme la science de la reconnaissance, de l’interprétation et de la simulation des émotions par la machine.</p>
     </body>
 <?php require 'footer.php'; ?>
--- nao.php
+++ nao.php
@@ -1,4 +1,5 @@
 <?php require 'header.php'; ?>
     <body>
+	<p>est le premier robot humanoïde développé par SoftBank Robotics (autrefois Aldebaran Robotics).Haut de 58cm NAO est un robot compagnon attachant, interactif et personnalisable.Chacun peut construire sa propre expérience avec des applications spécifiques selon son imagination et ses besoins.En Vendée, dans un centre spécialisé de l'association Autistes sans Frontière, NAO a aidé Lucas ,grâce à ses encouragement sans jugement, à l'apaiser et a facilité son contact avec les adultes.</p>
     </body>
 <?php require 'footer.php'; ?>
