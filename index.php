6da220aaa7c5e461df6481ca9940e9d57cad9cd9
TokiDEV <thomasboffy68@gmail.com>
2016-12-29 14:50:50 +0100

    Structure plus propre

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+ <meta charset="UTF-8"> 
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<?php require 'header.html'; ?>
+<body>
+	<h1>
+	<img src="nao.jpg" alt="nao"/> 
+		Alphago et La Robotique
+	<img src="Alphago.jpeg" alt="robot"/>
+	</h1>
+		<h2>Alphago</h2>
+		<p>
+		AlphaGo est un programme informatique capable de jouer au jeu de go, développé par l'entreprise britannique Google DeepMind.
+		En octobre 2015, il devient le premier programme à battre un joueur professionnel (le français Fan Hui) sur un goban de taille normale (19x19) sans handicap.
+		</p>
+	</body>
+<!--ceci est un commentaire -->
+</html>
