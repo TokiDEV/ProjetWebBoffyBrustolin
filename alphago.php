ac0c0101ff55337771f5f83eb85b4619bd2f0109
Plasminds <thomasboffy68@gmail.com>
2016-12-30 17:01:07 +0100

    Séparation en sections, ajout de texte pour alphago

--- alphago.php
+++ alphago.php
@@ -1,8 +1,22 @@
 <?php require 'header.php'; ?>
     <body>
         <div class="section">
-            <h1>AlphaGo</h1>
-            <p>AlphaGo est un programme informatique capable de jouer au jeu de go, développé par l'entreprise britannique Google DeepMind.En octobre 2015, il devient le premier programme à battre un joueur professionnel (le français Fan Hui) sur un goban de taille normale (19x19) sans handicap.Le terme est né en 1995 aux États-Unis dans un article publié par Rosalind Picard, une chercheuse du MIT qui considérait qu’il fallait que les robots nous ressemblent sur le plan émotionnel pour mieux nous servir. L’informatique affective peut se définir comme la science de la reconnaissance, de l’interprétation et de la simulation des émotions par la machine.</p>
+            <h1>Alpha Go</h1>
+            <div class="section">
+                <h2>Brève description</h2>
+                <p>Alpha Go est une IA conçue par l'entreprise DeepMind, filiale de Google. Comme son nom l'indique, l'IA a été créée afin de maîtriser le jeu de Go.</p>
+            </div>
+            <div class="section">
+                <h2>Alpha Go, en chiffres</h2>
+                <p>4 - 1 : il s'agit du résultat de sa confrontation avec Lee Sedol, le meilleur joueur du monde en mars 2016.</p>
+                <p>18/07/2016 : Alpha Go est considérée comme le meilleur joueur du monde, selon GoRatings.</p>
+                <p>10<sup>120</sup> : le nombre de Shannon, symbolisant la complexité des échecs (nombre de possibilités).</p>
+                <p>10<sup>600</sup> : complexité approximative du jeu de Go</p>
+            </div>
+            <div class="section">
+                <h2>Son prédecesseur</h2>
+                <p>DeepBlue est le précurseur d'Alpha Go. En 1997, cette IA a réussi à battre Gary Kasparov, le champion du monde d'échecs à cette époque.</p>
+            </div>
         </div>
     </body>
 <?php require 'footer.php'; ?>
--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 	<body>
 		<div class="section">
 			<h1>Alphago et La Robotique sociale</h1>
-			<h2>Alphago</h2>
-			<p>AlphaGo est un programme informatique capable de jouer au jeu de go, développé par l'entreprise britannique Google DeepMind.En octobre 2015, il devient le premier programme à battre un joueur professionnel (le français Fan Hui) sur un goban de taille normale (19x19) sans handicap.Le terme est né en 1995 aux États-Unis dans un article publié par Rosalind Picard, une chercheuse du MIT qui considérait qu’il fallait que les robots nous ressemblent sur le plan émotionnel pour mieux nous servir. L’informatique affective peut se définir comme la science de la reconnaissance, de l’interprétation et de la simulation des émotions par la machine.</p>
+			<p>Bienvenue sur ce site ! Vous y découvrirez plusieurs renseignements concernant AlphaGO et le domaine de la robotique sociale, deux projets tuteurés de cette année.</p>
 		</div>
 	</body>
 <?php require 'footer.php'; ?>
